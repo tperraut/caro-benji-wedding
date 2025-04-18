535f1b7d6b33770551e7c22aff3a8726b05880d3
Thomas Perraut <ths.prt@gmail.com>
2025-04-19 01:31:26 +0200

    refactor(assets): move

--- src/main.ts
+++ src/main.ts
@@ -13,22 +13,22 @@ kaplay({
 });
 
 loadRoot("./"); // A good idea for Itch.io publishing later
-loadSprite("startBG", "public/sprites/heading.png");
-loadSprite("forest", "public/sprites/forest.png");
-loadSprite("ollie", "public/sprites/ollie_run.png");
-loadSprite("ollie_jump", "public/sprites/ollie_jump.png");
-loadSprite("ollie_hit", "public/sprites/ollie_hit.png");
-loadSprite("strandmon", "public/sprites/strandmon.png");
+loadSprite("startBG", "sprites/heading.png");
+loadSprite("forest", "sprites/forest.png");
+loadSprite("ollie", "sprites/ollie_run.png");
+loadSprite("ollie_jump", "sprites/ollie_jump.png");
+loadSprite("ollie_hit", "sprites/ollie_hit.png");
+loadSprite("strandmon", "sprites/strandmon.png");
 
-loadSprite("sakiro_idle", "public/sprites/sakiro_idle.png");
-loadSprite("sakiro_hit", "public/sprites/sakiro_hit.png");
-loadSprite("broccoli", "public/sprites/broccoli.png");
-loadSprite("garnaal", "public/sprites/garnaal.png");
-loadSprite("kaas", "public/sprites/kaas.png");
-loadSprite("salami", "public/sprites/salami.png");
+loadSprite("sakiro_idle", "sprites/sakiro_idle.png");
+loadSprite("sakiro_hit", "sprites/sakiro_hit.png");
+loadSprite("broccoli", "sprites/broccoli.png");
+loadSprite("garnaal", "sprites/garnaal.png");
+loadSprite("kaas", "sprites/kaas.png");
+loadSprite("salami", "sprites/salami.png");
 
-loadSound("hit", "public/sounds/meow.wav");
-loadSound("eat", "public/sounds/pop.wav");
+loadSound("hit", "sounds/meow.wav");
+loadSound("eat", "sounds/pop.wav");
 
 setGravity(1000);
 createStartScene();
