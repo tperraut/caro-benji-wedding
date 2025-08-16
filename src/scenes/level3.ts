3a5a22bcf104e5a783e5de21b997001641c38916
Thomas Perraut <ths.prt@gmail.com>
2025-08-16 19:02:03 +0200

    refactor(level 3): put back timer to 3 mins

--- src/scenes/level3.ts
+++ src/scenes/level3.ts
@@ -526,7 +526,7 @@ export function createLevel3Scene() {
 
     let isMoving = false;
     let targetPos = vec2(0, 0);
-    let countdownTime = 150; // 2 minutes 30 seconds
+    let countdownTime = 180; // 3 minutes
 
     setGravity(0);
 
